eae50aa658aed216b84bce9ccf085617705282f0
calvin <ckodalo@gmail.com>
2022-10-22 17:25:43 +0300

    made progress

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
+class CashRegister
+    attr_reader :discount, :total
+    #attr_writer :add_item
+
+    def initialize(discount = 20)
+        @discount = discount
+        @total = 0
+    end
+
+    def total=(value)
+        @total  = value
+    end
+
+    def add_item(title, price)
+        total = @total + price
+    end
+
+
+end
 
