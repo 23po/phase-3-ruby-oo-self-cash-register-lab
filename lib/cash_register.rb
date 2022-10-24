3573b356f94a7a0eb597e9bdbc4aa21b51dbba4d
calvin <ckodalo@gmail.com>
2022-10-24 12:45:10 +0300

    commit tree a2306a008630f272b17d06a0303cb70c7c446f27

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,20 +1,49 @@
 class CashRegister
     attr_reader :discount, :total
-    #attr_writer :add_item
+    
 
-    def initialize(discount = 20)
+    def initialize(discount = 0)
         @discount = discount
         @total = 0
+        @items = []
     end
+
 
     def total=(value)
         @total  = value
     end
 
-    def add_item(title, price)
-        total = @total + price
+    def add_item(title, price, quantity=1)
+        #if self.discount 
+        #price = (self.discount/100) * price
+        #else
+        #price
+        #end
+        @last_transaction = [title, price, quantity]
+        quantity.times{@items << title}
+        
+        @total = @total + price * quantity 
+
+       
     end
 
+    def apply_discount
+        if @discount < 1
+            "There is no discount to apply."
+        
+        else
+        thediscount = @discount/100.0 * self.total
+        @total = @total - thediscount
+        "After the discount, the total comes to $#{@total.round}."
+        end
+    end
 
+    def items()
+        @items
+    end
+
+    def void_last_transaction 
+        @total = @total - (@last_transaction[1] * @last_transaction[2])
+    end
 end
 
